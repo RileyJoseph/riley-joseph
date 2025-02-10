637b91034e8bba784c00d1c7e72c2331889378be
Riley Joseph <riley.m.joseph1@gmail.com>
2025-02-10 13:25:29 -0600

    portfolio update

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -7,12 +7,50 @@ const portfolioItems = [
   { id: 4, name: "Patted", image: "/images/portfolio/patted.png" },
   { id: 5, name: "Solas", image: "/images/portfolio/solas-1.png" },
   { id: 6, name: "Coat", image: "/images/portfolio/coat.png" },
+  { id: 7, name: "OKC MoA", image: "/images/portfolio/moa.png" },
+  { id: 8, name: "guernsey", image: "/images/portfolio/guernsey.png" },
+  { id: 9, name: "Armstrong Auditorium", image: "/images/portfolio/armstrong.png" },
+  { id: 10, name: "Chisholm Creek", image: "/images/portfolio/ccreek.png" },
+  { id: 11, name: "First Med", image: "/images/portfolio/firstmed.png" },
+  { id: 12, name: "CBMC", image: "/images/portfolio/cbmc.png" },
+];
+
+const sassPortfolioItems = [  
+  { id: 1, name: "Frontera Software", image: "/images/portfolio/frontera.png" },
+  { id: 2, name: "Lennections", image: "/images/portfolio/lennections.png" },
 ];
 
 const Portfolio = () => {
   return (
 <div className="min-h-screen flex flex-col justify-center items-center p-6 portfolio">
   <h1 className="text-3xl font-bold text-center mb-8">My Portfolio</h1>
+
+  <h2>SaaS</h2>
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6">
+    {sassPortfolioItems.map((item) => (
+      <div 
+        key={item.id} 
+        className="relative group overflow-hidden rounded-lg shadow-lg cursor-pointer"
+      >
+        {/* Background Image */}
+        <img 
+          src={item.image} 
+          alt={item.name} 
+          className="client object-cover object-top w-full h-60 object-cover transition duration-300 group-hover:brightness-50"
+        />
+        
+        {/* Overlay with Text */}
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+          <h2 className="text-white text-2xl font-semibold">
+            {item.name}
+          </h2>
+        </div>
+      </div>
+    ))}
+  </div>
+
+  <h2>Web Development</h2>
+
 
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
     {portfolioItems.map((item) => (
