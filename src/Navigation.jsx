ea42ae0e1e9450c2ef3154004927a047e369b19a
Riley Joseph <riley.m.joseph1@gmail.com>
2025-02-11 14:46:30 -0600

    update colors not showing for other uses

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -12,7 +12,7 @@ const Navigation = ({ onColorChange }) => {
     <div className="navbar p-4">
       <nav className="flex justify-between items-center w-full">
         <div className="flex gap-4">          
-          <Link to="/" className="anti-btn py-2" onClick={handleCloseMenu}>Home</Link>   
+          <Link to="/" className="home-btn anti-btn py-2" onClick={handleCloseMenu}>Home</Link>   
         </div>
         <div className="relative">
           <button className="dropdown px-4 py-2 text-white rounded" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
