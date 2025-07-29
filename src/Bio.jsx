2b9a93b074df8441e3845aaf95da2eed0ab887d7
Riley Joseph <riley.m.joseph1@gmail.com>
2025-07-29 18:06:08 -0500

    bio update

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -5,11 +5,11 @@ function Bio() {
   const [error, setError] = useState('');
 
   return (
-<div className="min-h-screen p-8 flex flex-col items-center app-body all">
+<div className="min-h-screen p-8 flex flex-col app-body all">
   <div className="overlay"></div>
   <div className={`overlay glitch`}></div>    
 
-  <div id="bio" className="bio home-section flex flex-col md:flex-row items-center md:items-start">
+  <div id="bio" className="bio flex flex-col md:flex-row ">
     
     {/* Responsive Headshot Image */}
     <div className="bio-headshot w-full md:w-1/3 min-h-[300px] md:h-auto bg-cover bg-center bg-no-repeat"
@@ -17,20 +17,20 @@ function Bio() {
     </div>
 
     {/* About Me Section */}
-    <div className="info flex-1 p-8 md:p-16 text-center md:text-left">
-      <h2 className="name">About Me</h2>
+    <div className="info flex-1 p-4 md:p-16 text-center md:text-left">
+      <h2 className="name">ABOUT ME</h2>
+      <p className='subtitle pb-4'>Engineer, Artist, Nonprofit Professional</p>
       <p className="text-lg md:text-xl">
-        Software developer for almost a decade, I've worked at multiple startups, small and large all-purpose agencies, 
-        I've developed large enterprise software and managed databases with hundreds of thousands of users. 
-        I have experience working with clients and designers, communicating expectations, fielding concerns, 
-        building dynamic and responsive applications from design docs as well as helping to revise designs for optimization and cohesiveness.
+        Riley Joseph is the CTO of Proper Conduct, an all-purpose ad and branding agency in Oklahoma City and has 
+        been a software developer for over a decade. Riley has worked at multiple startups, small and large all-purpose agencies, 
+        developed large enterprise software and managed databases with hundreds of thousands of users.
       </p> 
       <br />
       <p className="text-lg md:text-xl">
-        I'm a graduate from the University of Oklahoma where I majored in Entrepreneurship 
-        and New Venture Management and minored in Non-Profit Organization Studies.
-        I'm on the Board of Directors for the Oklahoma Children's Theatre and am currently a member of 3 local bands 
-        (Coat, Trilogy, and Twiggs).
+        Living in Oklahoma City, Riley has been participating in the local arts community since 2016. 
+        He is currently Secretary on the Board of Directors for the Oklahoma Children's Theatre and a performing musician.
+        Riley is the guitarist for the bands Coat and Lover Girl. He is also a composer of soundscapes and electronic 
+        music and a practicing film photographer.
       </p>
     </div>
 
