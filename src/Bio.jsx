c8fbe95aa02208e9fe4956ff1149cecfde182f1c
Riley Joseph <riley.m.joseph1@gmail.com>
2025-07-29 21:09:22 -0500

    new bio image layout

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -18,20 +18,22 @@ function Bio() {
 
     {/* About Me Section */}
     <div className="info flex-1 p-4 md:p-16 text-center md:text-left">
-      <h2 className="name">ABOUT ME</h2>
+      <h2 className="name">ABOUT</h2>
       <p className='subtitle pb-4'>Engineer, Artist, Nonprofit Professional</p>
-      <p className="text-lg md:text-xl">
-        Riley Joseph is the CTO of Proper Conduct, an all-purpose ad and branding agency in Oklahoma City and has 
-        been a software developer for over a decade. Riley has worked at multiple startups, small and large all-purpose agencies, 
-        developed large enterprise software and managed databases with hundreds of thousands of users.
-      </p> 
-      <br />
-      <p className="text-lg md:text-xl">
-        Living in Oklahoma City, Riley has been participating in the local arts community since 2016. 
-        He is currently Secretary on the Board of Directors for the Oklahoma Children's Theatre and a performing musician.
-        Riley is the guitarist for the bands Coat and Lover Girl. He is also a composer of soundscapes and electronic 
-        music and a practicing film photographer.
-      </p>
+      <div className='bio-text'>
+        <p>
+          Riley Joseph is the CTO of Proper Conduct, an all-purpose ad and branding agency in Oklahoma City and has 
+          been a software developer for over a decade. Riley has worked at multiple startups, small and large all-purpose agencies, 
+          developed large enterprise software and managed databases with hundreds of thousands of users.
+        </p> 
+        <br />
+        <p>
+          Living in Oklahoma City, Riley has been participating in the local arts community since 2016. 
+          He is currently Secretary on the Board of Directors for the Oklahoma Children's Theatre and a performing musician.
+          Riley is the guitarist for the bands Coat and Lover Girl. He is also a composer of soundscapes and electronic 
+          music and a practicing film photographer.
+        </p>
+      </div>
     </div>
 
   </div>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -21,64 +21,65 @@ const Footer = () => {
   return (
 <footer className="bg-black text-white py-10 pulse-border">
   {/* full top footer */}
-  <div className="footer-content container mx-auto flex flex-col md:flex-row justify-between px-2 py-8">
+  <div className="footer-content container mx-auto flex flex-col md:flex-row justify-between py-8">
   
-  {/* Left Half: Procon */}
-  <div className="footer-details w-full md:w-1/2 flex flex-col px-4 md:px-8 sm:px-4">
-    <div className="footer-logos procon text-left">
-      <h2>Big Project?</h2>
-      <p>
-        While I can solve most of your coding problems, sometimes you want the full team at your back. Proper Conduct is an all-purpose agency that offers branding, marketing, security, AI integration and more.
-      </p>      
-      <a href="https://www.properconduct.com/" target="_blank" className="inline-block pt-2 procon-logo">
-        <img src="/images/procon-white.png" alt="Proper Conduct Logo" />
-      </a>  
-    </div>
-  </div>
-
-  {/* Right Half: Pages + Resume */}
-  <div className="footer-details w-full md:w-1/2 flex flex-col md:flex-row px-4 mt-8 md:mt-0 right-half">
-    
-    {/* Pages Section */}
-    <div className="text-left w-full md:w-1/2 mx-auto">
-      <p className="text-xl font-bold footer-heading">PAGES</p>
-      <ul className="text-sm space-y-1">
-        <li className="pt-2"><a href="/" className="hover:underline ">Home</a></li>
-        <li className="pt-2"><a href="/portfolio" className="hover:underline ">Portfolio</a></li>
-        <li className="pt-2"><a href="/bio" className="hover:underline ">About Me</a></li>       
-        <li className="pt-2"><a href="/contact" className="hover:underline ">Contact</a></li>          
-      </ul>
-    </div>
-
-    {/* Resume Section */}
-    <div className="footer-resume flex flex-col w-full md:w-1/2 text-left">
-      <p className="text-xl font-bold footer-heading md:text-left sm:text-left sm:pt-8 pt-8 md:pt-0">Resume With Password</p>
-      <p className="text-sm md:text-left sm:text-left">Password upon request.</p>
-
-      <div className="pt-4 w-full max-w-sm">
-        <input
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="px-4 py-2 border border-white bg-black text-white w-full text-center"
-        />
-        <br />
-        <button
-          onClick={handleDownload}
-          className="px-4 py-2 mt-2 bg-white text-black hover:bg-blue-600 w-full resume-download">
-          Download
-        </button>
-        {error && <p className="text-red-500 mt-2">{error}</p>}
+    {/* Left Half: Procon */}
+    <div className="footer-details w-full md:w-1/2 flex flex-col px-4 md:px-8 sm:px-4">
+      <div className="footer-logos procon text-left">
+        <h2>Big Project?</h2>
+        <p className="md:pr-8">
+          While I can solve most of your coding problems, sometimes you want the full team at your back. 
+          Proper Conduct is an all-purpose agency that offers branding, marketing, security, AI integration and more.
+        </p>      
+        <a href="https://www.properconduct.com/" target="_blank" className="inline-block pt-2 procon-logo">
+          <img src="/images/procon-white.png" alt="Proper Conduct Logo" />
+        </a>  
       </div>
     </div>
 
+    {/* Right Half: Pages + Resume */}
+    <div className="footer-details w-full md:w-1/2 flex flex-col md:flex-row px-4 mt-8 md:mt-0 right-half ">
+    
+      {/* Pages Section */}
+      <div className="text-left w-full md:w-1/2 mx-auto">
+        <p className="text-xl font-bold footer-heading">PAGES</p>
+        <ul className="text-sm space-y-1">
+          <li className="pt-2"><a href="/" className="hover:underline ">Home</a></li>
+          <li className="pt-2"><a href="/portfolio" className="hover:underline ">Portfolio</a></li>
+          <li className="pt-2"><a href="/bio" className="hover:underline ">About Me</a></li>       
+          <li className="pt-2"><a href="/contact" className="hover:underline ">Contact</a></li>          
+        </ul>
+      </div>
+
+      {/* Resume Section */}
+      <div className="footer-resume flex flex-col w-full md:w-1/2 text-left">
+        <p className="text-xl font-bold footer-heading md:text-left sm:text-left sm:pt-8 pt-8 md:pt-0">Resume With Password</p>
+        <p className="text-sm md:text-left sm:text-left">Password upon request.</p>
+
+        <div className="pt-4 w-full max-w-sm">
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="px-4 py-2 border border-white bg-black text-white w-full text-center"
+          />
+          <br />
+          <button
+            onClick={handleDownload}
+            className="px-4 py-2 mt-2 bg-white text-black hover:bg-blue-600 w-full resume-download">
+            Download
+          </button>
+          {error && <p className="text-red-500 mt-2">{error}</p>}
+        </div>
+      </div>
+
+    </div>
   </div>
-</div>
 
   {/* bottom footer */}
-  <div className="container mx-auto px-8">
-    <div className="footer-details w-full flex justify-end items-center space-x-4">
+  <div className="footer-content container mx-auto flex flex-col md:flex-row justify-between py-8">
+    <div className="footer-details w-full flex justify-end items-center space-x-4 ">
       <a href="https://www.instagram.com/r.m.joseph" target="_blank" className="text-white text-xl">
         <ion-icon name="logo-instagram"></ion-icon>
       </a>
@@ -90,9 +91,6 @@ const Footer = () => {
       </a>      
     </div>
   </div>
-
-
-
 
 </footer>
 
