6a160a18360bdbd4c1e72d8fe323cb0fd5ef38a2
Riley Joseph <riley.m.joseph1@gmail.com>
2025-08-12 18:42:34 -0500

    portfolio update wip

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -9,8 +9,8 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar p-4 relative">
-      <nav className="flex justify-between items-center w-full">
+    <nav className="navbar p-4 relative">
+      <div className="flex justify-between items-center w-full">
         <div className="flex gap-4">          
           <Link to="/" className="home-btn anti-btn py-2" onClick={handleCloseMenu}>Home</Link>   
         </div>
@@ -28,8 +28,7 @@ const Navbar = () => {
         <div className="md:hidden">
           <button 
             className="dropdown px-4 py-2 text-white rounded" 
-            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          >
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
             Menu
           </button>
         </div>
@@ -48,8 +47,8 @@ const Navbar = () => {
             <button className="text-white mt-6 text-xl" onClick={handleCloseMenu}>Close</button>
           </div>
         )}
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -24,23 +24,28 @@ const portfolioItems = [
 ];
 
 const sassPortfolioItems = [  
-  { id: 1, name: "Frontera Software", url:"frontera-software", image: "/images/portfolio/frontera.jpg", description: "Project details for Frontera Software." },
-  { id: 2, name: "Lennections", url:"lennections", image: "/images/portfolio/lennections.jpg", description: "Project details for Lennections." },
+  { id: 1, name: "Lennections", url:"lennections", image: "/images/portfolio/lennections.jpg", description: "Working as engineer, technology manager and team lead for a company developing software in the education sector. Developing new features and enhancements for the current codebase, Introducing new libraries, technology and systems to the application, Database management, fixing bugs, communicating with clients, supporting and maintaining the application, Managing small team of developers including setting tasks, helping grow their coding skills and code reviews, Meteor, MongoDB, Blaze.js, Node.js, AWS, OpenAI GPT-4 API, AI, machine learning, SaaS, JavaScript" },
+  { id: 2, name: "Frontera Software", url:"frontera-software", image: "/images/portfolio/frontera.jpg", description: "Engineering, building, and maintaining software that managed union members. Including over 15 locals with hundreds of thousands of members and many more records. Developed new features and enhancements, implemented stripe payment system. Meteor, MongoDB, Node.js, Vue, Docker, AWS, Stripe, SaaS, JavaScript" },  
 ];
 
 const Portfolio = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center p-6 portfolio all">
-      <h1 className="text-3xl font-bold text-center mb-8">My Portfolio</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">Software Development</h1>
       <h2>SaaS</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6">
+      <div className="flex flex-col w-full md:w-3/4">
         {sassPortfolioItems.map((item) => (
-          <Link key={item.id} className="relative group overflow-hidden rounded-lg shadow-lg cursor-pointer project">
-            <img src={item.image} alt={item.name} className="client object-cover object-top w-full h-60 transition duration-300 group-hover:brightness-50" />
-            <div className="title-overlay absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 opacity-60 md:opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-              <h2 className="text-white text-2xl font-semibold">{item.name}</h2>
+          <div className="flex flex-row">
+            <div key={item.id} className="relative group overflow-hidden rounded-lg shadow-lg cursor-pointer project  w-1/2">
+              <img src={item.image} alt={item.name} className="client object-cover object-top w-full h-60 transition duration-300 group-hover:brightness-50" />
+              <div className="title-overlay absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 opacity-60 md:opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                <h2 className="text-white text-2xl font-semibold">{item.name}</h2>
+              </div>
             </div>
-          </Link>
+            <div className="w-1/2">
+              {item.description}
+            </div>
+          </div>
         ))}
       </div>
       <h2>Full-Stack Development</h2>
